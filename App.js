24e50a06abbeda8bc9724c5926d40f396f303090
casil08 <156186206+casil08@users.noreply.github.com>
2024-08-19 17:30:19 +0800

    Fetch Data from an API in React

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import DataComponent from './DataComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      <DataComponent />
+    </div>
+  );
+}
+
+export default App;
